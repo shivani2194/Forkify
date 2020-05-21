ac862426716a2015c327227326eb4475ea2000ad
SHIVANI <shivanis2194@gmail.com>
2020-05-21 17:45:55 -0400

    New Error

---
+++ src/js/view/DOMelements.js
@@ -0,0 +1,6 @@
+export const element = {
+  searchButton: document.querySelector(".search"),
+  searchInput: document.querySelector('.search__field'),
+  resultList: document.querySelector('.results__list')
+  
+};
